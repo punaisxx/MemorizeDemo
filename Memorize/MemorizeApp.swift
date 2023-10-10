e0ad30136ae7cf28b504c457ce2b70d941063215
Punpun <rawin.nx@gmail.com>
2023-10-10 15:34:56 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Rawinnipha Thawornniwatsakun on 10/10/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
